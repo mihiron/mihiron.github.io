8bb370099c2e4ada8d33babff02ff250e8b618b7
mihiro <f.mihiroxx@gmail.com>
2022-08-24 22:11:04 +0900

    Delete EXAMPLE_PATH

--- components/alert.tsx
+++ components/alert.tsx
@@ -1,6 +1,5 @@
 import Container from './container'
 import cn from 'classnames'
-import { EXAMPLE_PATH } from '../lib/constants'
 
 type Props = {
   preview?: boolean
@@ -15,9 +14,8 @@ const Alert = ({ preview }: Props) => {
       })}
     >
       <Container>
-        <div className="py-2 text-center text-sm">
           {preview ? (
-            <>
+            <div className="py-2 text-center text-sm">
               This page is a preview.{' '}
               <a
                 href="/api/exit-preview"
@@ -26,20 +24,8 @@ const Alert = ({ preview }: Props) => {
                 Click here
               </a>{' '}
               to exit preview mode.
-            </>
-          ) : (
-            <>
-              The source code for this blog is{' '}
-              <a
-                href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
-                className="underline hover:text-blue-600 duration-200 transition-colors"
-              >
-                available on GitHub
-              </a>
-              .
-            </>
-          )}
-        </div>
+            </div>
+          ) : null}
       </Container>
     </div>
   )
