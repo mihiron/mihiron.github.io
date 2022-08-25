6918d39180beaf513e2ad2750835205a4e49b245
mihiro <f.mihiroxx@gmail.com>
2022-08-25 20:50:28 +0900

    Delete MoreStories

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,4 @@
 import Container from '../components/container'
-import MoreStories from '../components/more-stories'
 import HeroPost from '../components/hero-post'
 import Intro from '../components/intro'
 import Layout from '../components/layout'
@@ -14,7 +13,6 @@ type Props = {
 
 export default function Index({ allPosts }: Props) {
   const heroPost = allPosts[0]
-  const morePosts = allPosts.slice(1)
   return (
     <>
       <Layout>
@@ -32,7 +30,6 @@ export default function Index({ allPosts }: Props) {
               excerpt={heroPost.excerpt}
             />
           )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
         </Container>
       </Layout>
     </>
