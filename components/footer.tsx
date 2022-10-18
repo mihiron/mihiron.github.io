da50fefa9d4d8419a9e4625add38f1d18ff7c0f3
mihiro <f.mihiroxx@gmail.com>
2022-10-18 20:31:36 +0900

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,10 +4,21 @@ const Footer = () => {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
-        <div className="max-w-4xl mx-auto py-28 flex flex-col lg:flex-row items-center">
-          <h3 className="text-4xl lg:text-[2.5rem] tracking-tighter leading-tight text-center lg:text-left mb-10 lg:mb-0 lg:pr-4">
-            Statically Generated with Next.js.
+        <div className="max-w-4xl mx-auto py-10 items-center">
+          <h3 className="text-2xl tracking-tighter leading-tight text-center text-gray-500 mb-5">
+            COPYRIGHT © 2022 mihiron ALL RIGHTS RESERVED.
           </h3>
+          <div className="text-xl tracking-tighter leading-tight text-center text-gray-500">
+            The source code of this site is on&nbsp;
+            <a 
+              href='https://github.com/mihiron'
+              rel='noopener noreferrer'
+              className='hover:underline'
+            >
+              GitHub
+            </a>
+            .
+          </div>
         </div>
       </Container>
     </footer>
