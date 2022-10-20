aefaa480d9c17e7b466bd8b175c47e78c69da4d4
mihiro <f.mihiroxx@gmail.com>
2022-10-20 17:12:49 +0900

    Fix top page

--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -11,14 +11,46 @@ const HeroPost = ({
 }: Props) => {
   return (
     <section className="max-w-4xl mx-auto">
-      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
-        <div>
-          <h3 className="mb-4 text-4xl lg:text-5xl leading-tight">
-            <Link as={`/posts/${slug}`} href="/posts/[slug]">
-              <a className="hover:underline">{title}</a>
-            </Link>
-          </h3>
-        </div>
+      <div className="md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
+        <h3 className='mb-3 text-4xl'>
+          Profile
+        </h3>
+        <h4 className='mb-3 text-3xl'>
+          Name
+        </h4>
+        <p className='mb-10'>
+          Fumihiro Kinoshita（木下文宏）
+        </p>
+        <h4 className='mb-3 text-3xl'>
+          Links
+        </h4>
+        <h4 className="mb-3 text-3xl">
+          Resume
+        </h4>
+        <p className='mb-10'>
+          <Link as={`/posts/${slug}`} href="/posts/[slug]">
+            <a className="hover:underline">{title}</a>
+          </Link>
+        </p>
+        <h4 className="mb-3 text-3xl">
+          Jobs
+        </h4>
+        <p className='mb-10'>
+          Software Engineer, Engineering Manager, General Manager at Edridge inc. 2020/4 -
+        </p>
+        <h4 className="mb-3 text-3xl">
+          Degree
+        </h4>
+        <p className='mb-10'>
+          Bachelor of Environment and Information Studies at&nbsp;
+          <a 
+            href="https://www.keio.ac.jp/en/"
+            rel='noopener noreferrer'
+            className='hover:underline'
+          >
+            Keio University
+          </a>
+        </p>
       </div>
     </section>
   )
--- components/intro.tsx
+++ components/intro.tsx
@@ -2,8 +2,8 @@ import { SITE_NAME } from '../lib/constants'
 
 const Intro = () => {
   return (
-    <section className="max-w-4xl mx-auto flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-5xl md:text-7xl font-bold tracking-tighter leading-tight md:pr-8">
+    <section className="max-w-4xl mx-auto text-center mt-16 mb-16 md:mb-12">
+      <h1 className="text-5xl md:text-7xl font-bold tracking-tighter leading-tight">
         {SITE_NAME}
       </h1>
     </section>
