0e6e0c5dbea22da8d8cd0dba08fa95b36962385e
mihiro <f.mihiroxx@gmail.com>
2022-10-20 20:02:13 +0900

    Add fort awesome

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,14 +4,15 @@ const Footer = () => {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
-        <div className="max-w-4xl mx-auto py-10 items-center">
-          <h3 className="text-2xl tracking-tighter leading-tight text-center text-gray-500 mb-5">
+        <div className="max-w-4xl mx-auto py-8 items-center">
+          <h3 className="text-xl tracking-tighter leading-tight text-center text-gray-500 mb-2">
             COPYRIGHT © 2022 mihiron ALL RIGHTS RESERVED.
           </h3>
-          <div className="text-xl tracking-tighter leading-tight text-center text-gray-500">
+          <div className="text-l tracking-tighter leading-tight text-center text-gray-500">
             The source code of this site is on&nbsp;
             <a 
               href='https://github.com/mihiron'
+              target='_blank'
               rel='noopener noreferrer'
               className='hover:underline'
             >
--- components/hero-post.tsx
+++ components/hero-post.tsx
@@ -1,3 +1,5 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub, faLinkedin, faFacebook, faTwitter } from "@fortawesome/free-brands-svg-icons";
 import Link from 'next/link'
 
 type Props = {
@@ -12,7 +14,7 @@ const HeroPost = ({
   return (
     <section className="max-w-4xl mx-auto">
       <div className="md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
-        <h3 className='mb-3 text-4xl'>
+        <h3 className='mb-6 text-4xl'>
           Profile
         </h3>
         <h4 className='mb-3 text-3xl'>
@@ -24,6 +26,56 @@ const HeroPost = ({
         <h4 className='mb-3 text-3xl'>
           Links
         </h4>
+        <ul className="flex mb-10">
+          <li className="mr-3">
+            <a
+              href="https://github.com/mihiron"
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FontAwesomeIcon
+                icon={faGithub}
+                className='w-6 h-6'
+              />
+            </a>
+          </li>
+          <li className="mr-3">
+            <a
+              href="https://www.linkedin.com/in/fkinoshita/"
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FontAwesomeIcon
+                icon={faLinkedin}
+                className='w-6 h-6'
+              />
+            </a>
+          </li>
+          <li className="mr-3">
+            <a
+              href="https://www.facebook.com/fumihiro.kinoshita"
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FontAwesomeIcon
+                icon={faFacebook}
+                className='w-6 h-6'
+              />
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://twitter.com/mihiro___"
+              target='_blank'
+              rel='noopener noreferrer'
+            >
+              <FontAwesomeIcon
+                icon={faTwitter}
+                className='w-6 h-6'
+              />
+            </a>
+          </li>
+        </ul>
         <h4 className="mb-3 text-3xl">
           Resume
         </h4>
@@ -45,6 +97,7 @@ const HeroPost = ({
           Bachelor of Environment and Information Studies at&nbsp;
           <a 
             href="https://www.keio.ac.jp/en/"
+            target='_blank'
             rel='noopener noreferrer'
             className='hover:underline'
           >
