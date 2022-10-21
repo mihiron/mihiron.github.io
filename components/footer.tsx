1d077e920d55cee17877b0f4a32e7b5107290910
mihiro <f.mihiroxx@gmail.com>
2022-10-21 11:24:42 +0900

    Update footer.tsx

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,11 +4,11 @@ const Footer = () => {
   return (
     <footer className="bg-neutral-50 border-t border-neutral-200">
       <Container>
-        <div className="max-w-4xl mx-auto py-8 items-center">
-          <h3 className="text-xl tracking-tighter leading-tight text-center text-gray-500 mb-2">
+        <div className="max-w-4xl mx-auto py-6 items-center">
+          <h3 className="text-l tracking-tighter leading-tight text-center text-gray-500 mb-2">
             COPYRIGHT © 2022 mihiro ALL RIGHTS RESERVED.
           </h3>
-          <div className="text-l tracking-tighter leading-tight text-center text-gray-500">
+          <div className="text-m tracking-tighter leading-tight text-center text-gray-500">
             The source code of this site is on&nbsp;
             <a 
               href='https://github.com/mihiron'
